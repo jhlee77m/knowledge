22fea1be5836cab2e94cf46aa0e03b42c74d2f84
Jeonghee Lee <jhlee77m@gmail.com>
2022-04-24 07:50:42 +0900

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,3 @@
+import tensorflow as tf
+텐서 = tf.constant([3,4,5,6])
+print(텐서)
---
+++ practice.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+텐서 = tf.constant([3,4,5])
+텐서2 = tf.constant([6,7,8])
+print(텐서+텐서2)
+
+텐서4 = tf.zeros([2,2,3])
+print(텐서4)
+
+
